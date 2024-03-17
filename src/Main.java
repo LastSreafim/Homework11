494219cce799f6498a2e7f575fec6bd0c0d15d41
Nazar <epoxasmute@mail.ru>
2024-03-17 11:41:09 +0300

    Исправил замечания

--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,61 @@ import java.time.LocalDate;
 
 public class Main {
     public static void calculateYear(int year) {
-        if (year > 1584) {
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-                System.out.println(+year + " год вискосный");
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(+year + " год вискосный");
+        } else {
+            System.out.println(+year + " год не вискосный");
+        }
+    }
+
+    public static void choiceSystem(int oS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (oS == 0) {
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
             } else {
-                System.out.println(+year + " год не вискосный");
+                System.out.println("Установите версию приложения для iOS по ссылке");
             }
+
         } else {
-            System.out.println("Такого понятия как високосный год не существовало");
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+
         }
     }
 
-    public static void choiceSystem(int oS, int year) {
-        if (oS == 0 && year < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (oS == 1 && year < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (oS == 0 && year >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (oS == 1 && year >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Убедительная просьба! Избавьтесь от несертефицированного китайского девайса");
-        }
-    }
-
-    public static void calculateDeliveryTime(int deliveryDistance) {
+    public static int calculateDeliveryTime(int deliveryDistance) {
         if (deliveryDistance <= 20) {
-            System.out.println("Потребуется дней: 1.");
+            return 1;
         } else if (20 < deliveryDistance && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней: 2.");
+            return 2;
         } else if (60 < deliveryDistance && deliveryDistance < 100) {
-            System.out.println("Потребуется дней: 3.");
+            return 3;
         } else {
-            System.out.println("Доставки нет");
+            return 0;
         }
     }
 
     public static void main(String[] args) {
-        //task1     System.out.println("Task 1");
+        //task1
+        System.out.println("Task 1");
         calculateYear(2021);
-        //task2     System.out.println("Task 2");
-        int currentYear = LocalDate.now().getYear();
-        choiceSystem(0, currentYear);
+        //task2
+        System.out.println("Task 2");
+        int clientDeviceYear = 2022;
+        choiceSystem(0, clientDeviceYear);
         //task3
         System.out.println("Task 3");
-        calculateDeliveryTime(95);
+        int deliveryDistance = 245;
+        int deliveryTime = calculateDeliveryTime(deliveryDistance);
+        if (deliveryTime == 0) {
+            System.out.println("Доставки нет");
+        } else {
+            System.out.println("Потребуется дней: " + deliveryTime);
+        }
     }
 
 }
